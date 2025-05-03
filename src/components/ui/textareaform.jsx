8ba88afb33e9ff9bb5698cdf51b9ceacf11843f9
herbart hernandez <burnedcard77@gmail.com>
2025-05-02 19:10:09 -0700

    input text field

---
+++ src/components/ui/textareaform.jsx
@@ -0,0 +1,57 @@
+"use client";
+import { Button } from "./button";
+import { Textarea } from "./textarea";
+import { useState } from "react";
+
+export default function TextAreaForm() {
+  const [input, setInput] = useState("");
+  const [inputSize, setInputSize] = useState(0);
+  const [state, setState] = useState("default"); // default | loading
+
+  function handleInput(e) {
+    setInput(e.target.value);
+    setInputSize(e.target.value.length);
+  }
+
+  function handleKeyDown(e){
+    if(e.key==='Enter' && e.metaKey || e.ctrlKey)
+        e.target.form.requestSubmit();
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // setState("loading");
+      setInput("");
+      setInputSize(0);
+  }
+
+  return (
+    <form action={"#"} onSubmit={handleSubmit} className={` w-full h-full p-6`}>
+      <div
+        className={`transition-all focus-within:ring-2 border-2 rounded-lg max-h-fit p-2`}
+      >
+        <Textarea
+          placeholder={
+            "Tell me about your plans here (appointments, task, important dates). "
+          }
+          className={` max-h-72 border-none outline-none shadow-none focus-visible:ring-0 resize-none scroll`}
+          maxLength="500"
+          value={input}
+          onChange={handleInput}
+          disabled={state === "loading"}
+          onKeyDown={handleKeyDown}
+        ></Textarea>
+        <div className="bottom flex justify-between items-center">
+          <div>
+            <span className="text-xs align-text-bottom text-black/50">
+              {inputSize}/500
+            </span>
+          </div>
+          <Button disabled={state === "loading"} type="submit">
+            Shoot!
+          </Button>
+        </div>
+      </div>
+    </form>
+  );
+}
