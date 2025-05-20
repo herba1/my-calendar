9f238f16c8b5772e39faf9030f797045b135457f
herbart hernandez <burnedcard77@gmail.com>
2025-05-19 18:05:24 -0700

    full calendar implementation

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,15 @@
 import TaskCards from "@/app/components/task/TaskCards";
 import TextAreaForm from "@/app/components/ui/textareaform";
 import { useState, useEffect } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
 
 export default function Home() {
   const [tasks, setTasks] = useState([]);
   const [taskState, setTaskState] = useState("loading"); // default | loading
 
-  function setTaskStateProp(newState="default"){
+  function setTaskStateProp(newState = "default") {
     setTaskState(newState);
   }
 
@@ -18,6 +21,7 @@ export default function Home() {
         throw new Error(`response error ${response.status}`);
       }
       const data = await response.json();
+      console.log(data);
       setTasks(data);
       setTaskState("default");
     } catch (e) {
@@ -34,15 +38,69 @@ export default function Home() {
     fetchTask();
   };
 
+  const calendarTask = tasks.map((task) => {
+    let extra = {};
+    if(task.is_completed){
+      extra.textColor= "oklch(0.439 0 0)"
+
+
+    }
+    return {
+      id: task.task_id,
+      title: task.title,
+      start: task.due_at,
+      extendedProps:{
+      description: task.description,
+      },
+      backgroundColor:
+        task.priority === "high"
+          ? "#ef4444" // red-500
+          : task.priority === "medium"
+          ? "oklch(0.795 0.184 86.047)" // amber-500
+          : task.priority === "low"
+          ? "oklch(0.723 0.219 149.579)" // blue-500
+          : "#6b7280",
+      ...extra,
+    };
+  });
+  console.log(calendarTask);
+
   return (
     <div className=" h-full w-full flex flex-col ">
-      <TextAreaForm tasks={tasks} setTaskState={setTaskState} taskState={taskState} handleTaskChange={handleTaskChange} setTaskStateProp={setTaskStateProp}></TextAreaForm>
+      <TextAreaForm
+        tasks={tasks}
+        setTaskState={setTaskState}
+        taskState={taskState}
+        handleTaskChange={handleTaskChange}
+        setTaskStateProp={setTaskStateProp}
+      ></TextAreaForm>
       <TaskCards
         handleTaskChange={handleTaskChange}
         tasks={tasks}
         taskState={taskState}
         setTaskState={setTaskState}
       ></TaskCards>
+      <div className="w-dvw h-dvh flex justify-center items-center ">
+        <div className=" w-full h-full max-w-6xl p-2 ">
+          <FullCalendar
+            height={700}
+            plugins={[dayGridPlugin, timeGridPlugin]}
+            initialView="dayGridMonth"
+            headerToolbar={{
+              start: "today prev,next",
+              center: "title",
+              right: "dayGridMonth,timeGridDay",
+            }}
+            weekends={true}
+            events={calendarTask}
+            eventClick={(e)=>{
+              console.log(e.event);
+            }}
+            nowIndicator={true}
+            scrollTime={'08:00:00'}
+          />
+        </div>
+      </div>
     </div>
   );
 }
