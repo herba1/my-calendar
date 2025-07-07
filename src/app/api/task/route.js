bab9db6b13412bdcd419a652e4610597a24fb3b3
herba1 <burnedcard77@gmail.com>
2025-07-06 18:17:44 -0700

    wip

--- src/app/api/task/route.js
+++ src/app/api/task/route.js
@@ -1,15 +1,21 @@
 import { supabase } from "@/app/lib/supabase";
+import { createClient } from "@/app/utils/supabase/server";
 
 // get all task
 export async function GET() {
+  const supabase1 = await createClient()
+  const {data, error} = supabase1.auth.getUser();
+  console.log('user data',data);
+  console.log('user error',error);
   try {
-    const { data, error } = await supabase.from("task").select("*");
+    const { data, error } = await supabase1.from("task").select("*");
     if (data) {
+      console.log(data)
     }
     return Response.json(data);
   } catch (e) {
     console.error(e, error);
-    return Response.json({error:error})
+    return Response.json({ error: error });
   }
 }
 
@@ -18,16 +24,16 @@ export async function POST(request) {
   try {
     // should take in new task
     const newTask = await request.json();
-    console.log(`new task:`)
-    console.log(newTask)
+    console.log(`new task:`);
+    console.log(newTask);
     const { data, error } = await supabase
       .from("task")
       .insert(newTask)
       .select();
-      if(error){
-        console.error(error);
-        throw new Error(error);
-      }
+    if (error) {
+      console.error(error);
+      throw new Error(error);
+    }
 
     return Response.json(
       { data, error },
@@ -39,41 +45,39 @@ export async function POST(request) {
   }
 }
 
-export async function DELETE(request){
-    try{
-        const body = await request.json();
-        console.log(body);
-        const task_id = body.task_id
-        const { data,error } = await supabase
-            .from('task')
-            .delete()
-            .eq('task_id',task_id)
-        return Response.json({ message:"HELLO" },{ status:201})
-    }
-    catch(e){
-        console.error(e);
-        return Response.json({error: e.message}, { status:503})
-    }
-
+export async function DELETE(request) {
+  try {
+    const body = await request.json();
+    console.log(body);
+    const task_id = body.task_id;
+    const { data, error } = await supabase
+      .from("task")
+      .delete()
+      .eq("task_id", task_id);
+    return Response.json({ message: "" }, { status: 201 });
+  } catch (e) {
+    console.error(e);
+    return Response.json({ error: e.message }, { status: 503 });
+  }
 }
 
-export async function PUT(request){
-    try{
-        const body = await request.json();
-        console.log(`PUT:${body}`);
-        console.log(body);
-        const task_id = body.task_id;
-        const is_completed = body.is_completed;
-        const { data,error } = await supabase
-            .from('task')
-            .update({ ...body })
-            .eq('task_id',task_id)
-        return Response.json({ data,error },{ status:201})
-    }
-    catch(e){
-        console.error(e);
-        return Response.json({error: e, message:"Error PUT task"}, { status:503})
-    }
+export async function PUT(request) {
+  try {
+    const body = await request.json();
+    console.log(`PUT:${body}`);
+    console.log(body);
+    const task_id = body.task_id;
+    const is_completed = body.is_completed;
+    const { data, error } = await supabase
+      .from("task")
+      .update({ ...body })
+      .eq("task_id", task_id);
+    return Response.json({ data, error }, { status: 201 });
+  } catch (e) {
+    console.error(e);
+    return Response.json(
+      { error: e, message: "Error PUT task" },
+      { status: 503 }
+    );
+  }
 }
-
-
